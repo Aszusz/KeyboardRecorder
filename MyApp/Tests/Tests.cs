0e7e46db9bdd7061bb0a5ad7d6e65c502eeb119b
Unknown <adrianszuszkiewicz@gmail.com>
2018-05-25 00:53:54 +0200

    Added NUnit

---
+++ MyApp/Tests/Tests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
